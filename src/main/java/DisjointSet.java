4b21234006ea8137657681915f938a4bc02bae62
SamuelJCopeland <73198633+SamuelJCopeland@users.noreply.github.com>
2021-10-26 19:50:27 -0400

    Update DisjointSet.java

--- src/main/java/DisjointSet.java
+++ src/main/java/DisjointSet.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.*;
 
+//Index formula: let n = n index, N = max n, etc.
+//i = n + m * N + k * M * N
 
 public class DisjointSet {
     Map<Object, Object> rank = new HashMap<>();
@@ -29,7 +31,32 @@ public class DisjointSet {
     }
 
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws IOException{
+        java.io.InputStreamReader inputStream 
+                = new InputStreamReader(System.in);
+        java.io.BufferedReader stdin = new java.io.BufferedReader(inputStream);
+        
+        String nextLine = stdin.readLine();
+        
+        //Get the number of problems to solve.
+        int numProblems = Integer.parseInt(nextLine);
+        int N, M, K, l;
+        for(int i = 0; i < numProblems; i++){            
+            nextLine = stdin.readLine();
+            
+            //Get N, M, K, and L
+            String[] strNums = nextLine.split(" ");
+            
+            N = Integer.parseInt(strNums[0]);
+            M = Integer.parseInt(strNums[1]);
+            K = Integer.parseInt(strNums[2]);
+            l = Integer.parseInt(strNums[3]);
+            
+            //Iterate over the definitions of the new monarchies and store the
+            //dominions of each monarcy in an array e.g. monarchies[0][1] would
+            //represent the second dominion in the first monarchy.
+            
+        }
+        
     }
 }
