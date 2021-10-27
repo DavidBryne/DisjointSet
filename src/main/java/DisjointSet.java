36c1315553607c3b7480ce95e884c5995fa2afe2
SamuelJCopeland <73198633+SamuelJCopeland@users.noreply.github.com>
2021-10-27 09:14:23 -0400

    Update DisjointSet.java

--- src/main/java/DisjointSet.java
+++ src/main/java/DisjointSet.java
@@ -50,7 +50,12 @@ public class DisjointSet{
         }
     }
 
-    //Find what set a particular member is in
+    /**
+     * Find the root of a certain node and perform path compression
+     * 
+     * @param x Node to find root of
+     * @return root of the set
+     */
     public static Node findSet(Node x){
         if(x != x.getParent()){
             x.setParent(findSet(x.getParent()));
@@ -64,9 +69,9 @@ public class DisjointSet{
      * 
      * @param monarchies A 2d int array of the dominions in each monarchy
      * in chronological order of secession.
-     * @param N
-     * @param M
-     * @param K
+     * @param N N dimension
+     * @param M M dimension
+     * @param K K dimension
      * 
      * @return number of months separated
      */
@@ -93,43 +98,40 @@ public class DisjointSet{
                 = new InputStreamReader(System.in);
         java.io.BufferedReader stdin = new java.io.BufferedReader(inputStream);
         
-        String nextLine;// = stdin.readLine();
+        String nextLine;
         
         //Get the number of problems to solve.
-        int numProblems = 1;//Integer.parseInt(nextLine);
-        int[] answers = new int[numProblems];
+        int numProblems = 1;
+        int answer;
         int N, M, K, l;
-        for(int i = 0; i < numProblems; i++){            
-            nextLine = stdin.readLine();
-            
-            //Get N, M, K, and L
-            String[] strNums = nextLine.split(" ");
-            
-            N = Integer.parseInt(strNums[0]);
-            M = Integer.parseInt(strNums[1]);
-            K = Integer.parseInt(strNums[2]);
-            l = Integer.parseInt(strNums[3]);
-            
-            //Iterate over the definitions of the new monarchies and store the
-            //dominions of each monarcy in an array e.g. monarchies[0][1] would
-            //represent the second dominion in the first monarchy.
-            int[][] monarchies = new int[l][];
-            for(int j = 0; j < l; j++){
-                nextLine = stdin.readLine();
-                String[] dominionsAndNum = nextLine.split(" ");
-                int num = Integer.parseInt(dominionsAndNum[0]);
-                monarchies[j] = new int[num];
-                for(int k = 1; k <= num; k++){
-                    monarchies[j][k-1] = Integer.parseInt(dominionsAndNum[k]);
-                }
-            }
-            
-            answers[i] = solve(monarchies, N, M, K);
-        }
         
-        for(int i = 0; i < numProblems; i++){
-            System.out.println(answers[i]);
+        nextLine = stdin.readLine();
+            
+        //Get N, M, K, and L
+        String[] strNums = nextLine.split(" ");
+
+        N = Integer.parseInt(strNums[0]);
+        M = Integer.parseInt(strNums[1]);
+        K = Integer.parseInt(strNums[2]);
+        l = Integer.parseInt(strNums[3]);
+
+        //Iterate over the definitions of the new monarchies and store the
+        //dominions of each monarcy in an array e.g. monarchies[0][1] would
+        //represent the second dominion in the first monarchy.
+        int[][] monarchies = new int[l][];
+        for(int j = 0; j < l; j++){
+            nextLine = stdin.readLine();
+            String[] dominionsAndNum = nextLine.split(" ");
+            int num = Integer.parseInt(dominionsAndNum[0]);
+            monarchies[j] = new int[num];
+            for(int k = 1; k <= num; k++){
+                monarchies[j][k-1] = Integer.parseInt(dominionsAndNum[k]);
+            }
         }
+
+        answer = solve(monarchies, N, M, K);
+        
+        System.out.println(answer);
         
     }
 }
